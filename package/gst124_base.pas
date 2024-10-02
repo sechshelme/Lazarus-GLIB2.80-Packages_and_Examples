e3c13518e1354027250a60790250f6a843eb8b5b
tux <you@example.com>
2024-10-02 19:54:03 +0200

    gst

--- package/gst124_base.pas
+++ package/gst124_base.pas
@@ -29,15 +29,44 @@ type
   Tplanes_size_arr = array[0..(GST_VIDEO_MAX_PLANES) - 1] of Tgsize;
 
 
-  {$DEFINE read_interface}
-  {$include gst124_base_includes.inc}
-  {$UNDEF read_interface}
-
+{$DEFINE read_interface}
+{$include gst/base/gstaggregator.inc}                 // io.
+{$include gst/base/gstcollectpads.inc}                // io.
+{$include gst/base/gstadapter.inc}                    // io.
+{$include gst/base/gstbaseparse.inc}                  // io.
+{$include gst/base/gstbasetransform.inc}              // io.
+{$include gst/base/gstbasesrc.inc}                    // io.
+{$include gst/base/gstpushsrc.inc}                    // io. -> gstbasesrc
+{$include gst/base/gstbasesink.inc}                   // io.
+{$include gst/base/gstdataqueue.inc}                  // io.
+{$include gst/base/gstqueuearray.inc}                 // io.
+{$include gst/base/gsttypefindhelper.inc}             // io.
+{$include gst/base/gstflowcombiner.inc}               // io.
+{$include gst/base/gstbitreader.inc}                  // io.      viele Makros entfernt
+{$include gst/base/gstbitwriter.inc}                  // io.      viele Makros entfernt
+{$include gst/base/gstbytereader.inc}                 // io. -> gstbytewriter     viele Makros entfernt
+{$include gst/base/gstbytewriter.inc}                 // io.      viele Makros entfernt
+{$UNDEF read_interface}
 
 implementation
 
 {$DEFINE read_implementation}
-{$include gst124_base_includes.inc}
+{$include gst/base/gstaggregator.inc}                 // io.
+{$include gst/base/gstcollectpads.inc}                // io.
+{$include gst/base/gstadapter.inc}                    // io.
+{$include gst/base/gstbaseparse.inc}                  // io.
+{$include gst/base/gstbasetransform.inc}              // io.
+{$include gst/base/gstbasesrc.inc}                    // io.
+{$include gst/base/gstpushsrc.inc}                    // io. -> gstbasesrc
+{$include gst/base/gstbasesink.inc}                   // io.
+{$include gst/base/gstdataqueue.inc}                  // io.
+{$include gst/base/gstqueuearray.inc}                 // io.
+{$include gst/base/gsttypefindhelper.inc}             // io.
+{$include gst/base/gstflowcombiner.inc}               // io.
+{$include gst/base/gstbitreader.inc}                  // io.      viele Makros entfernt
+{$include gst/base/gstbitwriter.inc}                  // io.      viele Makros entfernt
+{$include gst/base/gstbytereader.inc}                 // io. -> gstbytewriter     viele Makros entfernt
+{$include gst/base/gstbytewriter.inc}                 // io.      viele Makros entfernt
 {$UNDEF read_implementation}
 
 end.
